add7b217fa5a62ed7995de5024623acddd8235d8
DemiPixel <luke5227@gmail.com>
2016-03-24 10:30:07 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+## TF2 Discord Bot
+
+A bot created by DemiPixel to run in the /r/tf2 discord channel
+
+### How to run
+
+#### Step 1
+
+Download and `npm install`.
+
+#### Step 2
+
+Go to the `config/` folder. Duplicate `template.json` and call it `default.json`. Edit the details as neccesary.
+
+#### Step 3
+
+Run `npm run default`
---
+++ src/data.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+module.exports = function(file, cb) {
+  var data;
+  var exists = true;
+  try {
+    fs.statSync(file);
+  } catch (e) {
+    exists = false;
+  }
+  if (!exists) {
+    data = { global: {}, users: {}};
+    saveData();
+  } else {
+    loadData(fs.readFileSync(file, 'utf8'));
+  }
+
+  var saving = false;
+  function saveData() {
+    if (saving) return;
+    saving = true;
+
+    fs.writeFile(file, JSON.stringify(data), (err) => {
+      if (err) console.log('[SAVING DATA ERR]',err);
+      saving = false;
+    });
+  }
+
+  function loadData(d) {
+    data = JSON.parse(d);
+  }
+
+  function global(str) {
+    return data.global[str];
+  }
+
+  function user(str) {
+    return data[str];
+  }
+
+  return {
+    global: data.global,
+    users: data.users,
+    save: saveData
+  }
+}
